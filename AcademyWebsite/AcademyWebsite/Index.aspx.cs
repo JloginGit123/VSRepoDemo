64c0f40cd9d36b1c4aeef762228a38e0ed01aaf0
Jayashree K R <Jayashree.kr@cognizant.com>
2017-09-12 13:35:34 +0530

    First Code

---
+++ AcademyWebsite/AcademyWebsite/Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AcademyWebsite
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("Hello World");//rectified
+            System.Console.WriteLine("Test US");//UST44 check in
+            System.Console.WriteLine("Test US");//UST7 check in
+            System.Console.WriteLine("Test Defect");//DEF4 check in5
+            System.Console.WriteLine("Test Defect");//DEF4 check Def6
+            System.Console.WriteLine("Commit for UST8");//DEF4 check
+            System.Console.WriteLine("Commit for UST9");//
+            System.Console.WriteLine("Commit for US1107");
+            System.Console.WriteLine("Commit for US1108");
+            System.Console.WriteLine("Commit for US996");
+            System.Console.WriteLine("Commit for US996");
+           
+            System.Console.WriteLine("Commit for US1110");
+            System.Console.WriteLine("Commit for US1111");
+            System.Console.WriteLine("Commit for US1111");
+            System.Console.WriteLine("Commit for US1102");
+            System.Console.WriteLine("Commit for US1120");
+            System.Console.WriteLine("Commit for US1120");
+
+            System.Console.WriteLine("Commit for BUG1179");//test
+        }
+    }
+}
